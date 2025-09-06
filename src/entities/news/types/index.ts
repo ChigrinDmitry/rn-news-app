0850ae4ab0ca3cf882f32d40add0175bd32710ba
Dmitry Chigrin <>
2025-09-06 19:01:36 +0500

    feat: add NewsArticle types

---
+++ src/entities/news/types/index.ts
@@ -0,0 +1,12 @@
+export interface NewsArticle {
+    title: string;
+    description: string;
+    content: string;
+    url: string;
+    urlToImage: string;
+    publishedAt: string;
+    source: {
+        name: string;
+    };
+    author: string;
+}
